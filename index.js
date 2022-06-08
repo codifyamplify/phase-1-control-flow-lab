f8c3165ddbce15de0312226432e322d5addc7a49
David Whitney <codifyamplify@gmail.com>
2022-06-08 12:46:42 -0400

    commit tree c9470723cd12a2cc3106a8529488757d63d706cc

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400 ) {
+    return 'This one is on me!'
+  } else if (someValue <= 2000) {
+    return 'That will be twenty bucks.'
+  } else if (someValue <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (someValue > 2500) {
+    return 'No can do.'
+  }
+}
+function ternaryCheckCity(providedCity) {
+  // Write your code here!
+  return providedCity === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(generosityFactor){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  switch (generosityFactor) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
+}``
